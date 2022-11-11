c27cdfd702e3834b4af5d36024dd24fd07b7e3d7
Jessica Jimantoro <jessica.jimantoro@agriaku.com>
2022-11-11 19:48:11 +0700

    change accept func return from widget to void

--- lib/widget.dart
+++ lib/widget.dart
@@ -14,58 +14,36 @@ class BlockWidget extends BlockElement {
         );
 
   @override
-  Widget? accept(NodeVisitor visitor) {
+  void accept(NodeVisitor visitor) {
     print('block widget $blockType visited!');
     List<Widget> widgets = <Widget>[];
 
     if (children != null) {
-      print('children != null');
       for (Node child in children!) {
         if (child is BlockWidget) {
           visitor.visitBlockElement(child);
         } else if (child is InlineWidget) {
-          widgets.add(Text('Inline ${child.inlineType}'));
+          widgets.add(
+            Text(child.rawText),
+          );
           visitor.visitInlineElement(child);
         }
       }
     }
-
-    if (blockType == BlockType.heading) {
-      return _headingWidget();
-    }
-
-    if (widgets.isNotEmpty) {
-      return Column(
-        children: widgets,
-      );
-    }
-    return null;
-  }
-
-  Widget? _headingWidget() {
-    if (rawText.startsWith('###')) {
-      rawText = rawText.replaceRange(0, 4, '');
-      return Text('h3: $rawText');
-    } else if (rawText.startsWith('##')) {
-      rawText = rawText.replaceRange(0, 3, '');
-      return Text('h2: $rawText');
-    } else if (rawText.startsWith('#')) {
-      rawText = rawText.replaceRange(0, 2, '');
-      return Text('h1: $rawText');
-    }
-    return null;
   }
 }
 
 class InlineWidget extends InlineElement {
+  String rawText;
+
   InlineWidget({
     required InlineType inlineType,
+    this.rawText = '',
   }) : super(inlineType: inlineType);
 
   @override
-  Widget accept(NodeVisitor visitor) {
+  void accept(NodeVisitor visitor) {
     print('inline widget $inlineType visited!');
-    return Text('Inline $inlineType');
   }
 }
 
@@ -76,18 +54,11 @@ class WidgetVisitor extends NodeVisitor {
 
   @override
   void visitBlockElement(BlockElement blockElement) {
-    final blockWidget = (blockElement as BlockWidget).accept(this);
-    if (blockWidget != null) {
-      _widgets.add(blockWidget);
-    }
+    (blockElement as BlockWidget).accept(this);
   }
 
   @override
-  void visitInlineElement(InlineElement inlineElement, [onlyOnce = false]) {
-    final inlineWidget = (inlineElement as InlineWidget).accept(this);
-
-    if (onlyOnce) {
-      _widgets = [inlineWidget];
-    }
+  void visitInlineElement(InlineElement inlineElement) {
+    (inlineElement as InlineWidget).accept(this);
   }
 }
